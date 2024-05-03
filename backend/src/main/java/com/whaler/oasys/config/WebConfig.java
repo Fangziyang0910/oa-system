ee6c8caff4cbb2a59d7dda2d2e54da22db02a430
whaler404 <whaler2842650960@163.com>
2024-05-03 14:28:33 +0800

    cors config

--- backend/src/main/java/com/whaler/oasys/config/WebConfig.java
+++ backend/src/main/java/com/whaler/oasys/config/WebConfig.java
@@ -2,6 +2,9 @@ package com.whaler.oasys.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +15,28 @@ public class WebConfig
 implements WebMvcConfigurer {
     private static final String[] EXCLUDE_PATH = {"/user/login", "/user/register", "/admin/login", "/admin/login"};
     private static final String API_PTTERN= "/user";
-    
+
+    @Bean
+    public CorsFilter corsFilter(){
+        // 跨域设置，允许浏览器访问 /API/** 的内容
+        UrlBasedCorsConfigurationSource source=
+        new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+            "/**", 
+            buildConfig()
+        );
+        return new CorsFilter(source);
+    }
+    private CorsConfiguration buildConfig(){
+        CorsConfiguration corsConfiguration=
+        new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor())
