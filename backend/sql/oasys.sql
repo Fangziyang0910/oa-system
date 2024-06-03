f2a314db61d7124fc360202327b6ccc37100f516
whaler404 <whaler2842650960@163.com>
2024-06-03 12:05:49 +0800

    解决merge问题

--- backend/sql/oasys.sql
+++ backend/sql/oasys.sql
@@ -278,8 +278,10 @@ DROP TABLE IF EXISTS `report`;
 CREATE TABLE `report` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `title` varchar(255) NOT NULL COMMENT '标题',
-  `content` varchar(255) NOT NULL COMMENT '内容',
-  `create_time` varchar(255) NOT NULL COMMENT '创建时间',
+  `content` varchar(8191) NOT NULL COMMENT '内容',
+  `create_time` date NOT NULL COMMENT '创建时间',
   `type` varchar(255) NOT NULL COMMENT '日报/周报',
   PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE(`create_time`,`type`)
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT ='报告' ROW_FORMAT = Dynamic;
